6587b1cae82f5d82c9b760937c5962edd6285f66
rahulgurnani <rahulgurnani09@gmail.com>
2018-10-02 23:30:06 +0530

    FirebaseUrl moved out to strings.xml

--- app/src/main/java/com/curefit/sensorapp/GlobalVariable.java
+++ app/src/main/java/com/curefit/sensorapp/GlobalVariable.java
@@ -15,14 +15,14 @@ public class GlobalVariable {
     private FirebaseStoreHelper firebaseStoreHelper;
     private User user;
     private static GlobalVariable globalVariable = null;
-    public static final String URL = "https://sensorapp-4fb6f.firebaseio.com";     // Firebase storage URL
+    public static String URL;     // Firebase storage URL
     private static Context context;
 
     /*
         Private contructor because the class is singelton.
      */
     private GlobalVariable() {
-        System.out.println("Global variable constructor called");
+        URL = getContext().getString(R.string.FIREBASE_URL);
         firebaseStoreHelper = new FirebaseStoreHelper(URL);
     }
 
